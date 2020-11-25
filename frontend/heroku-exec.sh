71acc4863c4fd4715c2cd6912d9daced92e36453
Prashant Srivastava <srivpra@amazon.com>
2020-11-25 07:20:06 -0800

    adding script to enable heroku ps:exec capability

---
+++ frontend/heroku-exec.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# see https://devcenter.heroku.com/articles/exec#enabling-docker-support
+[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
+
+set +o posix
