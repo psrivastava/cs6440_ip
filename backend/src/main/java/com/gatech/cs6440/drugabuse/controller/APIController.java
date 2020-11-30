7fde9ca89c30180dff145ee44becf8c2f87cdc63
Srivastava, Prashant <psrivastava48@gatech.edu>
2020-11-30 00:01:57 -0800

    api to return data csv

--- backend/src/main/java/com/gatech/cs6440/drugabuse/controller/APIController.java
+++ backend/src/main/java/com/gatech/cs6440/drugabuse/controller/APIController.java
@@ -9,8 +9,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -167,6 +177,25 @@ public class APIController {
         chatService.save(newChat);
         logger.info("Success: SAVED");
         return newChat;
+    }
+
+    @GetMapping(value="/reportdata/{yyyymm}", produces={"text/plain"})
+    public String getReportData(@PathVariable int yyyymm) {
+        logger.info("Hitting Chat endpoint");
+
+        try {
+            File file = ResourceUtils.getFile("classpath:data.csv");
+            InputStream in = new FileInputStream(file);
+            if (in != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                return (String)reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            } else {
+                throw new RuntimeException("resource not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
 }
